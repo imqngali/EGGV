d53c3dd28a2d53ccd17ac856e3575cd56006b1ee
imqngali <55797666+imqngali@users.noreply.github.com>
2021-11-17 18:22:48 +0600

    first-commit

---
+++ assets/js/app.js
@@ -0,0 +1,43 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (function() { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/assets/js/app.js":
+/*!******************************!*\
+  !*** ./src/assets/js/app.js ***!
+  \******************************/
+/***/ (function() {
+
+eval("$('.header_slider-slider').slick({\r\n    slidesToShow: 1,\r\n    slidesToScroll: 1,\r\n    autoplay: true,\r\n    arrows: false,\r\n    dots: true,\r\n    autoplaySpeed: 5000,\r\n    appendDots: $('.header_slider-dots')\r\n})\r\n\n\n//# sourceURL=webpack://brainscloud/./src/assets/js/app.js?");
+
+/***/ }),
+
+/***/ "./src/assets/js/file2.js":
+/*!********************************!*\
+  !*** ./src/assets/js/file2.js ***!
+  \********************************/
+/***/ (function() {
+
+eval("console.log('file 2');\r\n\n\n//# sourceURL=webpack://brainscloud/./src/assets/js/file2.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	__webpack_modules__["./src/assets/js/app.js"]();
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/assets/js/file2.js"]();
+/******/ 	
+/******/ })()
+;
